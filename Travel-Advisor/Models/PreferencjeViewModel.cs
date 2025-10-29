9afc7a0b98b4b50a8d206d25828def62c52c5058
Anna Trybel <annakalinovich717@gmail.com>
2025-10-29 17:41:17 +0100

    Add PreferencjeViewModel class for travel preferences management

---
+++ Travel-Advisor/Models/PreferencjeViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Travel_Advisor.Models
+{
+    public class PreferencjeViewModel
+    {
+        [Required]
+        [Display(Name = "Maksymalny budżet na osobę (PLN)")]
+        [Range(500, 20000)]
+        public int BudzetPln { get; set; } = 5000;
+
+        [Required]
+        [Display(Name = "Jak długo chcesz podróżować?")]
+        public string Czas { get; set; }
+
+        [Required]
+        [Display(Name = "Jaki jest główny cel Twojej podróży?")]
+        public string StylPodrozy { get; set; }
+
+        [Required]
+        [Display(Name = "Jakie otoczenie preferujesz?")]
+        public string Otoczenie { get; set; }
+
+        [Required]
+        [Display(Name = "Z kim podróżujesz?")]
+        public string SkladGrupy { get; set; }
+    }
+}
