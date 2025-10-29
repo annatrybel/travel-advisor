36af38ebaff80de671554e274ed7fe66a1770938
Anna Trybel <annakalinovich717@gmail.com>
2025-10-29 17:41:37 +0100

    Add ErrorViewModel class to handle error information in the application

---
+++ Travel-Advisor/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Travel_Advisor.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
