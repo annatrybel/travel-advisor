a02b879d9f95027e24eb8ee81f0c8571ea70edc9
Anna Trybel <annakalinovich717@gmail.com>
2025-10-29 17:41:54 +0100

    Add RekomendacjaViewModel class for recommendation data representation

---
+++ Travel-Advisor/Models/RekomendacjaViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Travel_Advisor.Models
+{
+    public class RekomendacjaViewModel
+    {
+        public string Tytul { get; set; }
+        public string Opis { get; set; }
+        public string Uzasadnienie { get; set; }
+        public string ImageUrl { get; set; }
+        public List<string> Szczegoly { get; set; } = new List<string>();
+    }
+}
