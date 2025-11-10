373eee57db06f077fd2d8a7eef5e13df8ea58f94
Anna Trybel <annakalinovich717@gmail.com>
2025-11-10 17:19:25 +0100

    Add HomeController with Index and Privacy actions for handling user preferences

---
+++ Travel-Advisor/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Travel_Advisor.Models;
+
+namespace Travel_Advisor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new PreferencjeViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(PreferencjeViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var rekomendacja = PobierzRekomendacje(model);
+
+            return View("Results", rekomendacja);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
+
+
