729f157ecc204626a2a46940d2abec2af263cb59
Anna Trybel <annakalinovich717@gmail.com>
2025-11-10 17:18:36 +0100

    Refactor DestinationSeeder to update destination categories and enhance API request handling

--- Travel-Advisor/Models/DestinationSeeder.cs
+++ Travel-Advisor/Models/DestinationSeeder.cs
@@ -28,8 +28,8 @@ namespace Travel_Advisor.Models
             }
 
             var kurortyTureckie = await FindPotentialDestinations("beach.beach_resort,beach", 36.8969, 30.7133, 500000);
-            var miastaEuropejskie = await FindPotentialDestinations("tourism.sights,historic", 50.0755, 14.4378, 800000);
-            var miejscaNaturalne = await FindPotentialDestinations("natural,leisure.national_park", 49.2992, 19.9496, 300000);
+            var miastaEuropejskie = await FindPotentialDestinations("tourism.sights", 50.0755, 14.4378, 800000);
+            var miejscaNaturalne = await FindPotentialDestinations("leisure.park,tourism.attraction.natural", 49.2992, 19.9496, 300000);
 
             var wszystkieMiejsca = kurortyTureckie
                 .Concat(miastaEuropejskie)
@@ -55,10 +55,27 @@ namespace Travel_Advisor.Models
             if (string.IsNullOrEmpty(geoapifyApiKey)) return potentialDestinations;
 
             var client = _httpClientFactory.CreateClient();
+            string url;
+            if (categories.Contains("beach") || categories.Contains("sights"))
+            {
+                url = string.Format(CultureInfo.InvariantCulture,
+                  "https://api.geoapify.com/v2/places?categories={0}&bias=proximity:{1},{2}&lang=pl&limit=20&apiKey={3}",
+                  Uri.EscapeDataString(categories), lon, lat, geoapifyApiKey);
 
-            var url = string.Format(CultureInfo.InvariantCulture,
-               "https://api.geoapify.com/v2/places?categories={0}&filter=circle:{1},{2},{3}&lang=pl&limit=20&apiKey={4}",
-               Uri.EscapeDataString(categories), lon, lat, radius, geoapifyApiKey);
+                if (categories.Contains("beach"))
+                {
+                    url = string.Format(CultureInfo.InvariantCulture,
+                      "https://api.geoapify.com/v2/places?categories={0}&filter=circle:{1},{2},{3}&lang=pl&limit=20&apiKey={4}",
+                      Uri.EscapeDataString(categories), lon, lat, radius, geoapifyApiKey);
+                }
+            }
+            else 
+            {
+                url = string.Format(CultureInfo.InvariantCulture,
+                  "https://api.geoapify.com/v2/places?categories={0}&bias=proximity:{1},{2}&limit=20&apiKey={3}",
+                  Uri.EscapeDataString(categories), lon, lat, geoapifyApiKey);
+            }
+
 
             try
             {
@@ -72,27 +89,21 @@ namespace Travel_Advisor.Models
                     foreach (var feature in json["features"])
                     {
                         var props = feature["properties"];
-                        List<string> travelStyles = new List<string>();
-                        List<string> environments = new List<string>();
+                        List<string> travelStyles;
+                        List<string> environments;
+                        List<string> durations;
+                        List<string> groupTypes;
 
                         switch (categories)
                         {
-                            case "tourism.resort,beach":
-                                travelStyles = new List<string> { "odpoczynek" };
-                                environments = new List<string> { "plaze" };
-                                break;
-                            case "tourism.city,historic":
-                                travelStyles = new List<string> { "kultura", "rozrywka" };
-                                environments = new List<string> { "miasto" };
-                                break;
-                            case "natural_area,national_park":
-                                travelStyles = new List<string> { "przygoda", "odpoczynek" };
-                                environments = new List<string> { "natura" };
-                                break;
+                            case "beach.beach_resort,beach": 
+                                travelStyles = new List<string> { "odpoczynek" }; environments = new List<string> { "plaze" }; durations = new List<string> { "tydzien", "dwa_tygodnie" }; groupTypes = new List<string> { "para", "rodzina", "znajomi" }; break;
+                            case "tourism.sights":
+                                travelStyles = new List<string> { "kultura", "rozrywka" }; environments = new List<string> { "miasto" }; durations = new List<string> { "weekend", "tydzien" }; groupTypes = new List<string> { "solo", "para", "znajomi" }; break;
+                            case "leisure.park,tourism.attraction.natural":
+                                travelStyles = new List<string> { "przygoda", "odpoczynek" }; environments = new List<string> { "natura" }; durations = new List<string> { "weekend", "tydzien", "dwa_tygodnie" }; groupTypes = new List<string> { "solo", "para", "rodzina", "znajomi" }; break;
+                            default: travelStyles = new List<string>(); environments = new List<string>(); durations = new List<string>(); groupTypes = new List<string>(); break;
                         }
-
-                        var durations = new List<string> { "weekend", "tydzien", "dwa_tygodnie" };
-                        var groupTypes = new List<string> { "solo", "para", "rodzina", "znajomi" };
 
                         var newDest = new Destination
                         {
